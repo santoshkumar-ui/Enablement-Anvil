387ba90b50bc5251af916448bb6aededf441371c
santoshbshivade@gmail.com <santoshbshivade@gmail.com>
2024-07-05 10:43:30 +0000

    Created Admin.ManageUsers.ManageEmployee.editemployee.ItemTemplate1; Edited Admin.ManageUsers.ManageEmployee.editemployee; Deleted Employee.EditEmployee

--- client_code/Admin/ManageUsers/ManageEmployee/editemployee/ItemTemplate1/__init__.py
+++ client_code/Admin/ManageUsers/ManageEmployee/editemployee/ItemTemplate1/__init__.py
@@ -1,11 +1,11 @@
-from ._anvil_designer import EditEmployeeTemplate
+from ._anvil_designer import ItemTemplate1Template
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class EditEmployee(EditEmployeeTemplate):
+class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
