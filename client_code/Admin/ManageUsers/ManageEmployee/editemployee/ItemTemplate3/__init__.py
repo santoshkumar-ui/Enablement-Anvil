70a966522788355f0180f8f2d666cb0a2558bbde
santoshbshivade@gmail.com <santoshbshivade@gmail.com>
2024-07-08 08:21:25 +0000

    Created Admin.ManageUsers.ManageEmployee.editemployee.ItemTemplate4 and Admin.ManageUsers.ManageEmployee.editemployee.ItemTemplate3; Edited Admin.ManageUsers.ManageEmployee.editemployee, settings and Admin.ManageUsers.ManageEmployee.editemployee.Form1

--- client_code/Admin/ManageUsers/ManageEmployee/editemployee/ItemTemplate3/__init__.py
+++ client_code/Admin/ManageUsers/ManageEmployee/editemployee/ItemTemplate3/__init__.py
@@ -1,19 +1,13 @@
-from ._anvil_designer import editemployeeTemplate
+from ._anvil_designer import ItemTemplate3Template
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class editemployee(editemployeeTemplate):
+class ItemTemplate3(ItemTemplate3Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1.items = app_tables.employee.search(usertype='employee')
-
 
     # Any code you write here will run before the form opens.
-
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
