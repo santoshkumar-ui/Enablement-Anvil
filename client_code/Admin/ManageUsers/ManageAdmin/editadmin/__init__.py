ba9cd3bd8ec7294a30aed91d3572797e90434696
deepanshukori21@gmail.com <deepanshukori21@gmail.com>
2024-07-08 08:47:15 +0000

    Created Admin.ManageUsers.ManageAdmin.editadmin.ItemTemplate5, Admin.ManageUsers.ManageAdmin.editadmin.ItemTemplate4 and Admin.ManageUsers.ManageAdmin.editadmin.ItemTemplate3; Edited Admin.ManageUsers.ManageAdmin.editadmin

--- client_code/Admin/ManageUsers/ManageAdmin/editadmin/__init__.py
+++ client_code/Admin/ManageUsers/ManageAdmin/editadmin/__init__.py
@@ -10,10 +10,15 @@ class editadmin(editadminTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.repeating_panel_1.items = app_tables.admin.search(usertype='employee')
 
     # Any code you write here will run before the form opens.
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    anvil.server.call('store_user_input', self.email, self.name, self.password)
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('Admin.ManageUsers.ManageAdmin')
+    
     
