87a698e668f02a7db238c119cfdb4f40488d757b
ygnanendraprasad@gmail.com <ygnanendraprasad@gmail.com>
2024-07-09 09:26:57 +0000

    Edited Admin.ManageContent, Admin.ManageUsers, Admin.ManageUsers.ManageAdmin, Admin.ManageUsers.ManageAdmin.addadmin and 14 more

--- client_code/Admin/ManageUsers/ManageAdmin/editadmin/ItemTemplate4/__init__.py
+++ client_code/Admin/ManageUsers/ManageAdmin/editadmin/ItemTemplate4/__init__.py
@@ -1,9 +1,6 @@
 from ._anvil_designer import ItemTemplate4Template
 from anvil import *
 import anvil.server
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 
 
 class ItemTemplate4(ItemTemplate4Template):
