bc8123c1778406b0122b0a06bc55fccf52952db1
santoshbshivade@gmail.com <santoshbshivade@gmail.com>
2024-07-09 11:26:32 +0000

    Edited Admin.ManageUsers.ManageEmployee.editemployee and Admin.ManageUsers.ManageEmployee.editemployee.ItemTemplate1

--- client_code/Admin/ManageUsers/ManageEmployee/editemployee/__init__.py
+++ client_code/Admin/ManageUsers/ManageEmployee/editemployee/__init__.py
@@ -10,13 +10,10 @@ class editemployee(editemployeeTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.repeating_panel_1.items = app_tables.users.search(user_type='employee')
+    
 
 
     # Any code you write here will run before the form opens.
-
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    alert('Your Details Has Been Saved')
 
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
