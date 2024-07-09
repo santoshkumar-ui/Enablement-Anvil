9fdc9ba22be1bd9c03b2dea573f7bfdc44d9af82
santoshbshivade@gmail.com <santoshbshivade@gmail.com>
2024-07-09 09:08:02 +0000

    Edited Admin.ManageUsers.ManageEmployee.editemployee and Admin.ManageUsers.ManageEmployee.editemployee.ItemTemplate1; Deleted Admin.ManageUsers.ManageEmployee.editemployee.Form1, Admin.ManageUsers.ManageEmployee.editemployee.Form1.ItemTemplate2, Admin.ManageUsers.ManageEmployee.editemployee.ItemTemplate4 and Admin.ManageUsers.ManageEmployee.editemployee.ItemTemplate3

--- client_code/Admin/ManageUsers/ManageEmployee/editemployee/ItemTemplate1/__init__.py
+++ client_code/Admin/ManageUsers/ManageEmployee/editemployee/ItemTemplate1/__init__.py
@@ -6,5 +6,15 @@ class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.repeating_panel_1.items = app_tables.users.serach(user_type='employee')
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert('Your Details Has Been saved')
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.item.delete()
+    self.remove_from_parent()
--- client_code/Admin/ManageUsers/ManageEmployee/editemployee/__init__.py
+++ client_code/Admin/ManageUsers/ManageEmployee/editemployee/__init__.py
@@ -6,11 +6,15 @@ class editemployee(editemployeeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1.items = app_tables.employee.search(usertype='employee')
+    self.repeating_panel_1.items = app_tables.users.search(user_type='employee')
 
 
     # Any code you write here will run before the form opens.
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    pass
+    alert('Your Details Has Been Saved')
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('Admin.ManageUsers.ManageEmployee.editemployee')
